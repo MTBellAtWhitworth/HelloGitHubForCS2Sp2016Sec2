0e58b9631821ad27819738528e01aef288efb2be
MTBellAtWhitworth <mbell@whitworth.edu>
2016-02-16 13:18:47 -0800

    V.1 of Hello GitHub

---
+++ HelloGithub/HelloGithub/Source.cpp
@@ -0,0 +1,12 @@
+//Created by M. Bell to illustrate how to post something to GitHub!
+//Last updated 2/15/2016
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, GitHub!" << endl;
+	return 0;
+}
+
